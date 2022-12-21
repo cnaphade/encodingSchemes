671f9c8040a3cf437e12b78adc7a3217aba9390f
Chaitanya Naphade <chaitanya.naphade@gmail.com>
2022-12-21 07:55:19 -0800

    binary to hex conversion added

--- utf-8/python/utf-8.py
+++ utf-8/python/utf-8.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 def test(text, decoded_codepoints):
 	native_func_codepoints = ""
@@ -11,7 +12,28 @@ def test(text, decoded_codepoints):
 		print("Algorithm Works!")
 	else:
 		print("Failure!")
+
+def binary_to_hex(binary_char):
+	hexadecimal = ""
+	mapping = {}
+	for i in range(16):
+		if i < 10:
+			mapping[i] = str(i)
+		else:
+			mapping[i] = chr(ord('a') + (i - 10))
+
+	nibbles = re.findall('....?',binary_char[::-1])
+
+	for i in range(len(nibbles)):
+		decimal = 0
+		for j in range(len(nibbles[i])):
+			decimal += int(nibbles[i][j]) * (2 ** j)
+		hexadecimal = mapping[decimal] + hexadecimal
 	
+	idx = 0
+	while(hexadecimal[idx] == '0'):
+		idx += 1
+	return hexadecimal[idx:]	
 
 def utf_8_decode(text):
 	octet = 8
@@ -29,7 +51,7 @@ def utf_8_decode(text):
 			for j in range(1, num_bytes + 1):
 				binary_char += encoded_char_bytes[(octet * j) + 2 : octet * (j + 1)]
 				
-		decoded_codepoints += str(hex(int(binary_char, 2))) + " "
+		decoded_codepoints += "0x" + binary_to_hex(binary_char) + " "
 
 	print(decoded_codepoints)	
 	test(text, decoded_codepoints)
